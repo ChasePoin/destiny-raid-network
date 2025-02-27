a37aac8ade0c05b2f0b12e6ffede614958062db6
Chase Poindexter <mcstarcraft04@gmail.com>
2025-02-26 19:47:01 -0600

    learned what asynchronous was...

--- grabbers.py
+++ grabbers.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import main
+import asyncio
+import aiohttp
 
 HEADERS = {'X-API-KEY': os.getenv("bungie_token"), 'Content-Type': 'application/json'}
 
@@ -14,76 +16,63 @@ class RootPlayer():
         """
         if '#' not in username:
             raise Exception("Please include the # with the 4 digits in the name.")
-        
+        self.username = username
         # need to split numbers and name to properly and accurately perform user search
-        splitString = username.split("#")
-        displayName = splitString[0]
-        displayNameCode = splitString[1]
-        if displayNameCode[0] == "0":
-            displayNameCode = displayNameCode[1:]
+        splitString = self.username.split("#")
+        self.displayName = splitString[0]
+        self.displayNameCode = splitString[1]
+        if self.displayNameCode[0] == "0":
+            self.displayNameCode = self.displayNameCode[1:]
 
-        print(displayName + ' ' + displayNameCode)
+        print(self.displayName + ' ' + self.displayNameCode)
+    
+    async def setup(self):
+        async with aiohttp.ClientSession() as session:
+            await self.get_starting_info(session)
+            await self.get_character_ids(session)
+            await self.get_instance_ids(session)
+            await self.get_other_players_in_activities(session)
+    
+    async def fetch(self, session, url):
+        async with session.get(url, headers=HEADERS) as response:
+            return await response.json()  
 
-        # deprecated ?
+    async def get_starting_info(self, session):
         try: 
-            url = f'https://www.bungie.net/Platform/User/Search/Prefix/{displayName}/0/'
-            response = requests.get(url, headers=HEADERS)
-            response = response.json()
+            url = f'https://www.bungie.net/Platform/User/Search/Prefix/{self.displayName}/0/'
+            response = await self.fetch(session, url)
         except:
             raise Exception("Error when searching user. Make sure the name and digits are correct.")
-
-        # try:
-        #     url = f'https://www.bungie.net/Platform/User/Search/GlobalName/{pagenum}/'
-        #     name = {'displayNamePrefix': displayName}
-        #     response = requests.post(url, json=name, headers=HEADERS)
-        #     response = response.json()
-        # except:
-        #     raise Exception("Error when searching user AGAIN.")
-        
-        print(response)        
-        # get prefix and numbers, important for verifying correct user
 
         # get any user with that prefix, then specifically search for the user with the numbers provided
         listOfUsers = response['Response']['searchResults']
         for user in listOfUsers:
-            if user['bungieGlobalDisplayName'] == displayName and user['bungieGlobalDisplayNameCode'] == int(displayNameCode):
+            if user['bungieGlobalDisplayName'] == self.displayName and user['bungieGlobalDisplayNameCode'] == int(self.displayNameCode):
                 # need bungie id, destiny id, platform for instance ids
                 self.platform              =  user['destinyMemberships'][0]['membershipType']
                 self.destiny_membership_id =  user['destinyMemberships'][0]['membershipId']
-                self.bungie_name           =  username
-
-        self.character_ids = self.get_character_ids(self.platform, self.destiny_membership_id)
-        self.instance_ids = self.get_instance_ids(self.destiny_membership_id,self.platform,self.character_ids)
-        self.players_raided_with = self.get_other_players_in_activities(self.instance_ids)
-                
-            
+                self.bungie_name           =  self.username
+                      
 
 
-
-    def get_character_ids(self, platform, destiny_membership_id) -> list:
+    async def get_character_ids(self, session) -> list:
         """"
         Gets character ids due to instance ids being stored per character.
 
         """
         try:
-            url = f"https://www.bungie.net/Platform/Destiny2/{platform}/Profile/{destiny_membership_id}/?components=Profiles,Characters"
-            response = requests.get(url, headers=HEADERS)
+            url = f"https://www.bungie.net/Platform/Destiny2/{self.platform}/Profile/{self.destiny_membership_id}/?components=Profiles,Characters"
+            response = await self.fetch(session, url)
         except:
-            print("Unable to get character information. Exiting...")
-            exit()
+            raise Exception("character issue")
             # exits because there is no point if character ids can not be accessed
+
+        self.character_ids = response['Response']['profile']['data']['characterIds']
         
-        profileData = response.json()
-
-        data = profileData['Response']['profile']['data']
-        # general profile data
-
-        all_character_ids =  data['characterIds']
-
-        return all_character_ids
 
 
-    def get_instance_ids(self, destiny_membership_id, platform, character_ids) -> list:
+
+    async def get_instance_ids(self, session) -> list:
         """
         Uses the activity history endpoint in order to get all raid instance ids for a player.
 
@@ -93,37 +82,38 @@ class RootPlayer():
         Returns:
             list: All instance ids of raids for a player.
         """
-        page_num = 0
+        
         instance_ids = []
+        tasks = []
         # limit to 50 pages for now
-        for character in character_ids:
+        for character in self.character_ids:
             for page_num in range (0,50):
                 try:
-                    url = f"https://www.bungie.net/Platform/Destiny2/{platform}/Account/{destiny_membership_id}/Character/{character}/Stats/Activities/?mode=4&page={page_num}"
-                    response = requests.get(url, headers=HEADERS)
+                    url = f"https://www.bungie.net/Platform/Destiny2/{self.platform}/Account/{self.destiny_membership_id}/Character/{character}/Stats/Activities/?mode=4&page={page_num}"
+                    tasks.append(self.fetch(session, url))
                 except: 
                     raise Exception("Error while trying to request instance ids for raids.")
 
-                response = response.json()
+        responses = await asyncio.gather(*tasks)
+        
 
-                # this means we're done
-                if response['Response'] == {}:
-                    # print(len(instance_ids)) total successfully finished raids
-                    break
-                
+        # this means we're done
+        for response in responses:        
+            if response['Response'] == {}:
+                pass # print(len(instance_ids)) # total successfully finished raids
+            else:
                 # probably a good idea to store a dictionary of a player with their instance ids in case of duplicate look ups? maybe, will just do an array for now
                 activities = response['Response']['activities']
 
                 # move through every single instance of a raid, only adding succesfully completed raids
-                for i, _ in enumerate(activities):
-                    instanceId = activities[i]['activityDetails']['instanceId']
-                    if activities[i]['values']['completed']['basic']['value'] == 1.0:
+                for activity in activities:
+                    instanceId = activity['activityDetails']['instanceId']
+                    if activity['values']['completed']['basic']['value'] == 1.0:
                         instance_ids.append(instanceId)
-            
-        return instance_ids
+        self.instance_ids = instance_ids
     
 
-    def get_other_players_in_activities(self, instance_ids):
+    async def get_other_players_in_activities(self, session):
         """
         Converts a list of instance ids into a list of players this user has succesfully completed a raid with.
 
@@ -131,23 +121,23 @@ class RootPlayer():
             instance_ids (list): List of player's raid instance ids.
         """
         player_dictionary = dict()
+        tasks = []
         
-        for activity_id in instance_ids:
+        for activity_id in self.instance_ids:
             try:
                 url = f"https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/{activity_id}/"
-                response = requests.get(url, headers=HEADERS)
+                tasks.append(self.fetch(session, url))
             except:
                 raise Exception(f"Issue with getting activity data for {activity_id}.")
-            response = response.json() 
 
+        responses = await asyncio.gather(*tasks)
             # we only want full completions, can skip checkpoints
+        for response in responses:
             from_checkpoint = response['Response']['activityWasStartedFromBeginning']
             # print(from_checkpoint)
             if from_checkpoint == True:
 
                 other_players = response['Response']['entries']
-
-                # FOR LATER: STORE USED INSTANCE IDS TO SPEED UP PROCESSOR FOR OTHER PLAYERS
                 
                 for player in other_players:
                     player_data = player['player']['destinyUserInfo']
@@ -163,24 +153,26 @@ class RootPlayer():
                         else:
                             player_dictionary[player_data['membershipId']] = [player_data['bungieGlobalDisplayName'] + "#" + str(player_data["bungieGlobalDisplayNameCode"]), 1, player_data['membershipType']]
 
-                    # keep track of each instance's activity id and players within it
-                    if activity_id in main.inst_dictionary:
-                        main.inst_dictionary[activity_id].append(player_data['membershipId'])
-                    else:
-                        main.inst_dictionary[activity_id] = [player_data['membershipId']]
-        return player_dictionary
+                # keep track of each instance's activity id and players within it
+                # if activity_id in main.inst_dictionary:
+                #     main.inst_dictionary[activity_id].append(player_data['membershipId'])
+                # else:
+                #     main.inst_dictionary[activity_id] = [player_data['membershipId']]
+        self.players_raided_with = player_dictionary
                                                                       
 class AdjacentPlayer(RootPlayer):
     def __init__(self, username, destiny_membership_id, platform):
+        print(username, destiny_membership_id, platform)
         self.platform              =  platform
         self.destiny_membership_id =  destiny_membership_id
         self.bungie_name           =  username
+    
+    async def setup(self):
+        async with aiohttp.ClientSession() as session:
+            await self.get_character_ids(session)
+            await self.get_instance_ids(session)
+            await self.get_other_players_in_activities(session) 
 
-        self.character_ids = self.get_character_ids(self.platform, self.destiny_membership_id)
-        self.instance_ids = self.get_instance_ids(self.destiny_membership_id,self.platform,self.character_ids)
-        self.players_raided_with = self.get_other_players_in_activities(self.instance_ids)   
 
 
-# TO DO: ADD GLOBAL INSTANCE TRACKER TO NOT REDO DATA
-# TO DO: CHANGE INIT TO TAKE PLATFORM, MEMBERSHIP ID; CHANGE GET_OTHER_PLAYERS TO PUT PLATFORM IN THE DICTIONARY
                 
--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import grabbers
+import asyncio
 
 inst_dictionary = dict()
 
-def main():
+async def main():
     # ask for username then create an object of the player
     name = input("Enter the username of the player you want to add. Example: Khazicus#9648: ")
     player = grabbers.RootPlayer(name)
-    print(player.players_raided_with.items())
-    sorted_data = dict(sorted(player.players_raided_with.items(), key=lambda x: x[1][1], reverse=True))
+    await player.setup()
+    root_user_sorted = dict(sorted(player.players_raided_with.items(), key=lambda x: x[1][1], reverse=True))
 
-    print(sorted_data)
-    for element in sorted_data:
-        print(sorted_data[element][0] + " : " + str(sorted_data[element][1]))
+    for element in root_user_sorted:
+        print(root_user_sorted[element][0] + " : " + str(root_user_sorted[element][1]))
     
     # contains users connected to me and users connected to them
     full_user_dict = dict()
-    full_user_dict[name] = sorted_data
+    full_user_dict[name] = root_user_sorted
 
-    for other_player in sorted_data:
-        print(sorted_data[other_player][0])
-        player_obj = grabbers.AdjacentPlayer(sorted_data[other_player][0], other_player, sorted_data[other_player][2])
-        sorted_data = dict(sorted(player_obj.players_raided_with.items(), key=lambda x: x[1][1], reverse=True))
-        full_user_dict[player_obj.bungie_name] = sorted_data
+    for other_player in root_user_sorted:
+        try:
+            print(root_user_sorted[other_player][0])
+            player_obj = grabbers.AdjacentPlayer(root_user_sorted[other_player][0], other_player, root_user_sorted[other_player][2])
+            await player_obj.setup()
+            new_sort = dict(sorted(player_obj.players_raided_with.items(), key=lambda x: x[1][1], reverse=True))
+            full_user_dict[player_obj.bungie_name] = new_sort
+        except:
+            print(f"failed to get data for {root_user_sorted[other_player][0]}")
     
-    for user in full_user_dict:
-        print(user + "has raided with " + full_user_dict[user][0] + " " + 
-              full_user_dict[user][1] + " times.")  
+    print(full_user_dict)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
